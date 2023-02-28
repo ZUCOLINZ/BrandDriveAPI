1d3060b0497471b57374fc8ce8c791700a842dda
ZUCOLINZ <collinsizuchukwu18@gmail.com>
2023-02-28 17:42:30 +0100

    first commit

---
+++ src/products/product.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common/decorators";
+
+import { ProductsController } from "./products.controller";
+import { ProductsService } from "./products.service";
+
+@Module({
+  controllers: [ProductsController],
+  providers: [ProductsService],
+})
+
+export class ProductModule{}
