11b609eaaf0764194d8b13b681e1de1f7b59f570
ZUCOLINZ <collinsizuchukwu18@gmail.com>
2023-03-01 15:45:05 +0100

    likuu

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException} from "@nestjs/common";
 
 
 import {Product} from './product.model';
@@ -10,13 +10,21 @@ export class ProductsService{
   private products: Product[] = [];
 
   insertProduct( title: string , description: string , price: number,) {
-    const prodId = new Date().toString();
+    const prodId = Math.random ().toString();
    const newProduct = new Product(prodId, title, description, price);
    this.products.push(newProduct);
    return prodId;
   }
-
-  getProducts(){
-    return this.productsService.getProducts();
+  getProducts() {
+    return [...this.products];
   }
-}
+
+  getSingleProduct(productId: string){
+    const product = this.products.find((prod) => prod.id == productId);
+    if (!product) {
+      throw new NotFoundException('could not find product');
+    }
+    return {...product};
+  }
+
+  }
