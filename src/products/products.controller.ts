b56ef9d74b3da6f37e1e83828aa21403f30f245c
ZUCOLINZ <collinsizuchukwu18@gmail.com>
2023-03-01 09:31:30 +0100

    zucolinz

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, Get } from "@nestjs/common";
 import { ProductsService } from "./products.service";
 @Controller('products')
 
@@ -23,5 +23,10 @@ export class ProductsController{
     return {id: generateId}; 
 
 
+  }
+  @Get()
+  getAllProducts() {
+    return this.productsService.getProducts();
+    
   }
 }
